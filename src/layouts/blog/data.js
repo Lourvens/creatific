98c71f2dec4c298a6b62100d612cac34f9ae8d71
lourvens <lourvens.luxamar@gmail.com>
2022-08-29 21:34:45 +0200

    large screen webstie

---
+++ src/layouts/blog/data.js
@@ -0,0 +1,25 @@
+import blog_Img_1 from './assets/blog1.png'
+import blog_Img_2 from './assets/blog2.png'
+import blog_Img_3 from './assets/blog3.png'
+
+const blogDetails = [
+  {
+    desc: "secret behind being creative without any strategy",
+    date: "12 jul 2021",
+    service: "design",
+    img: blog_Img_1
+  },
+  {
+    desc: "The simpliest form of design Gives minimalism look",
+    date: "12 jul 2021",
+    service: "design",
+    img: blog_Img_2
+  },
+  {
+    desc: "Being creative while inventing ideas is quick startup",
+    date: "12 jul 2021",
+    service: "business",
+    img: blog_Img_3
+  }
+]
+export default blogDetails
